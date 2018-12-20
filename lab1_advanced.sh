54b5f53adc3c26c564f342c6386411f3e2a09057
Faller Artem Sergeevich <faller@ccfit.nsu.ru>
2018-12-20 20:33:01 +0700

    initial

--- lab1_advanced.sh
+++ lab1_advanced.sh
@@ -4,3 +4,10 @@
 #Poisk failov .txt
 find . -name "*.txt"
 grep 'Friday' crusoe.txt
+#Pishem v text fail hello world
+echo HelloWorld  > info.txt
+echo HelloWorld >> info.txt
+#Dobavili v fail
+#Vivedem spisok vailof v directorii git
+ls >> info.txt
+
