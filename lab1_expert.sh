cfba25c33e29b1623402d2ead27290d94415e7a7
Faller Artem Sergeevich <faller@ccfit.nsu.ru>
2018-12-20 21:35:39 +0700

    initial

--- lab1_expert.sh
+++ lab1_expert.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #Kolvo friday
-grep -o 'Friday' crusoe.txt |  wc -w > info3.txt
-sed s/Friday/Saturday/ crusoe.txt
+grep -o 'Friday' crusoe.txt.1 |  wc -w > info3.txt
+sed 's/Friday/Saturday/g'  crusoe.txt.1 > crusoe.txt
+
